a19bc2ab132060591af05d8140639ec673a1ef97
UsmanAliAshraf <usmanaliashraf.uet@gmail.com>
2024-12-16 22:55:29 +0500

    initialized socket.io

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const http = require('http');
 const socket= require('socket.io');
-const {chess, Chess} = require('chess.js');
+const {Chess} = require('chess.js');
+const path = require('path');
+const { title } = require('process');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,5 +17,15 @@ app.set('view engine', 'ejs');
 app.use(express.static (path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Chess Game'});
+});
+
+
+
+io.on("connection", (uniqueSocket) => {
+    console.log("Connected");
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
